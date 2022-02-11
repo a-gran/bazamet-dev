fc4ad82c4ec8c2d3c434936005701e1fc433e69d
Aristarh Gran <aristarhgran@gmail.com>
2022-02-11 15:05:49 +0300

    fixed footer

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ lightGallery(document.getElementById('animated-thumbnails-gallery-1'), {
     thumbnail: true,
 });
 
+
 lightGallery(document.getElementById('animated-thumbnails-gallery-2'), {
     thumbnail: true,
 });
