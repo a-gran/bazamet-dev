1a5fcb56856be651db66c4a7a619f5e5e621d798
Aristarh Gran <aristarhgran@gmail.com>
2022-02-16 13:55:25 +0300

    make adaptive

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// галерея
+
 lightGallery(document.getElementById('animated-thumbnails-gallery-1'), {
     thumbnail: true,
 });
@@ -6,3 +8,24 @@ lightGallery(document.getElementById('animated-thumbnails-gallery-1'), {
 lightGallery(document.getElementById('animated-thumbnails-gallery-2'), {
     thumbnail: true,
 });
+
+// меню-гамбургер
+
+const hamb = document.querySelector("#hamb");
+const popup = document.querySelector("#popup");
+const menu = document.querySelector("#menu").cloneNode(1);
+const body = document.body;
+
+hamb.addEventListener("click", hambHandler);
+
+function hambHandler(e) {
+    e.preventDefault();
+    popup.classList.toggle("open");
+    hamb.classList.toggle("active");
+    body.classList.toggle("noscroll");
+    renderPopup();
+}
+
+function renderPopup() {
+    popup.appendChild(menu);
+}
